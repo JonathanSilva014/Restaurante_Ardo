164037464d036d7b3088209cfbbc536ea1e8df48
Jonathan Silva <Jonathanrosadiaz94@gmail.com>
2022-04-01 23:47:58 +0100

    Create contatos.php

---
+++ contatos.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <title>ORDO</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <!-- jQuery e Bootstrap -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
+    <!-- Nosso CSS e JS -->
+    <link rel="stylesheet" href="css/style.css">
+    <script src="js/scripts.js"></script>
+</head>
+
+<body>
+    
+<nav id="barra_navegacao" class="navbar navbar-inverse">
+	<div class="container-fluid">
+		<ul class="nav navbar-nav">
+			<li><a href="index.php">Home</a></li>
+			<li><a href="menu.php">Menu</a></li>
+			<li class="active"><a href="contatos.php">Contatos</a></li>
+			<li><a href="login.php">Gerência</a></li>
+		</ul>
+	</div>
+</nav>
+
+<div class="container">
+    <div class="row">
+        <div class="col-xs-12 col-md-4">
+            <h3><b><u>CONTACTOS</u></b></h3> 
+            <br>
+            <h5><b>MORADA</b></h5> 
+            <p>Rua do Mosteiro, Parque das Varas S/N<br>
+            4465-703 Leça do Balio
+            <br></p>
+            <br>
+            <h5><b>TELEFONE</b></h5>
+            <p>Restaurante: 967 761 110<br>
+            <br></p>
+            <h5><b>E-MAIL</b></h5>
+            <p>geral@bhfoz.pt<br>
+            <br></p>
+            <div>
+            <a href="https://www.instagram.com/ordobhconcept/"><img class="footer_icons" src="img/instagram.png" alt="Instagram"></a>
+            <a href="https://www.facebook.com/ORDO.BH.Concept"><img class="footer_icons" src="img/facebook.png" alt="Facebook"></a>
+            <a href="https://www.zomato.com/pt/porto/ordo-bh-concept-le%C3%A7a-do-balio"><img class="footer_icons" src="img/zoomato.png" alt="Zoomato"></a>
+        </div>
+        </div>
+        <div class="col-xs-12 col-md-8 visible-sm visible-md visible-lg">
+            <div class="mapouter"><div class="gmap_canvas"><iframe width="600" height="400" id="gmap_canvas" src="https://maps.google.com/maps?q=ordo%20bh%20concept&t=&z=11&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><a href="https://www.embedgooglemap.net">embed google map</a></div><style>.mapouter{position:relative;text-align:right;height:400px;width:600px;}.gmap_canvas {overflow:hidden;background:none!important;height:400px;width:600px;}</style></div>        </div>   
+        </div>
+</div>       
+</body>
+</html>
